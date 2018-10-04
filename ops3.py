b90d7010cca33b12bbd53b4876be4d112567ab94
Aces14 <42877956+Aces14@users.noreply.github.com>
2018-10-04 13:24:27 -0700

    Add files via upload

---
+++ ops3.py
@@ -0,0 +1,20 @@
+#ops3.py jca 
+x = 7
+y = 7
+print("7 == 7",x == y)
+print("7 != 7",x != y)
+print("7 > 7",x > y)
+print("7 < 7",x < y)
+print("7 >= 7",x >= y)
+print("7 <= 7",x <= y)
+
+"""
+Operator 	Name 	Example 	Try it
+== 	Equal 	x == y 	
+!= 	Not equal 	x != y 	
+> 	Greater than 	x > y 	
+< 	Less than 	x < y 	
+>= 	Greater than or equal to 	x >= y 	
+<= 	Less than or equal to 	x <= y 	
+
+"""
